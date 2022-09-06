f9a212aa417d7b7222caf3be6dddd0ee80ce81b9
chrisphum <76226574+chrisphum@users.noreply.github.com>
2022-09-05 22:23:01 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash 
 
+echo Hello, insert a value for N:  
+read varname
 
-cd Prog1O0 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+
+
+cd Prog1O0 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
 
-cd Prog1O3 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+cd Prog1O3 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
 
-cd Prog2O0 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+cd Prog2O0 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
 
-cd Prog2O3 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+cd Prog2O3 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
 
-cd Prog3O0 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+cd Prog3O0 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
 
-cd Prog3O3 && mkdir build && cd build && cmake ../ && make && ./code 10000000
+cd Prog3O3 && mkdir build && cd build && cmake ../ && make && ./code $varname
 cd .. && rm -rf build && cd ..
